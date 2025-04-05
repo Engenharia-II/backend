b946761c1883ac251c1e72cd616067c1c8831bd7
Andrey Dantas <andreydantasvf@gmail.com>
2025-04-05 13:43:23 -0300

    Feat: use databaseConnection instace in repository

--- src/infrastructure/repositories/example.repository.ts
+++ src/infrastructure/repositories/example.repository.ts
@@ -1,20 +1,49 @@
 import { UserInterface } from '@/domain/interfaces/users.interface';
+import { DatabaseConnection } from '../database/connection';
+import { PrismaClient } from '@prisma/client';
 
-class ExampleRepository {
+export class ExampleRepository {
+  private db: PrismaClient;
+
   constructor() {
-    // this.db = db
+    this.db = DatabaseConnection.getInstance().getClient();
   }
 
   async save({ name, email }: UserInterface): Promise<UserInterface | void> {
-    console.log('Saving user to database');
-    return {
-      name,
-      email
-    };
+    try {
+      await this.db.user.create({
+        data: {
+          name,
+          email
+        }
+      });
+      return {
+        name,
+        email
+      };
+    } catch (error) {
+      throw new Error('Error saving user to database: ' + error);
+    }
   }
 
-  async findOne(payload: string): Promise<UserInterface | void> {
-    return console.log('Finding user in database');
+  async findAll() {
+    try {
+      const users = await this.db.user.findMany();
+      return users;
+    } catch (error) {
+      throw new Error('Error finding users in database: ' + error);
+    }
+  }
+
+  async findById(id: string) {
+    try {
+      const user = await this.db.user.findUnique({
+        where: { id }
+      });
+      return user;
+    } catch (error) {
+      throw new Error('Error finding user by id in database: ' + error);
+    }
   }
 }
 
