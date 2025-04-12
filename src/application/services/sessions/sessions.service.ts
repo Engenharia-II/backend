2433110e6a64f124069afd81f08a68cc2dad486f
Andrey Dantas <andreydantasvf@gmail.com>
2025-04-12 19:52:07 -0300

    Feat: create login service

--- src/application/services/sessions/sessions.service.ts
+++ src/application/services/sessions/sessions.service.ts
@@ -1,6 +1,7 @@
 import PasswordHash from '@/application/security/bcrypt';
 import { USER_DEFAULT_ROLE_ID } from '@/infrastructure/config/constants';
 import userRepository from '@/infrastructure/repositories/user.repository';
+import { AppError } from '@/infrastructure/webserver/app-error';
 
 export const signUp = async (name: string, email: string, password: string) => {
   try {
@@ -13,6 +14,27 @@ export const signUp = async (name: string, email: string, password: string) => {
       roleId: USER_DEFAULT_ROLE_ID
     });
   } catch (error) {
-    throw new Error(error as string);
+    throw new AppError(error as string, 400);
+  }
+};
+
+export const login = async (email: string, password: string) => {
+  try {
+    const user = await userRepository.findByEmail(email);
+    if (!user) {
+      throw new AppError('E-mail ou senha inválidos', 401);
+    }
+
+    const isPasswordValid = await PasswordHash.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new AppError('E-mail ou senha inválidos', 401);
+    }
+
+    return user;
+  } catch (error) {
+    if (error instanceof AppError) {
+      throw error;
+    }
+    throw new AppError('Erro ao realizar login', 500);
   }
 };
