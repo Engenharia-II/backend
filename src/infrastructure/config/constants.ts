5aa613fd484ce09023157c60e0833ed7926244c0
Andrey Dantas <andreydantasvf@gmail.com>
2025-04-12 19:47:22 -0300

    Feat: create and configure auth plugin

--- src/infrastructure/config/constants.ts
+++ src/infrastructure/config/constants.ts
@@ -1,5 +1,11 @@
 const config = {
-  app: { domain: '0.0.0.0', port: 3333, kind: 'developement' }
+  app: { domain: '0.0.0.0', port: 3333, kind: 'developement' },
+  jwt: { secret: '1q@w3e4r5t6y', expires: '24h' },
+  cookie: {
+    secret: '1q@w3e4r5t6y',
+    expires: '24h',
+    cookieName: 'token'
+  }
 };
 
 export const USER_DEFAULT_ROLE_ID = 1;
---
+++ src/infrastructure/plugins/auth.plugin.ts
@@ -0,0 +1,46 @@
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import fastifyJwt from '@fastify/jwt';
+import fastifyCookie from '@fastify/cookie';
+import fastifyPlugin from 'fastify-plugin';
+import config from '@/infrastructure/config/constants';
+import { AppError } from '../webserver/app-error';
+
+export default fastifyPlugin(
+  async (fastify) => {
+    fastify.register(fastifyCookie);
+    fastify.register(fastifyJwt, {
+      secret: config.jwt.secret,
+      cookie: {
+        cookieName: config.cookie.cookieName,
+        signed: false
+      }
+    });
+
+    fastify.decorate(
+      'authenticate',
+      async function (
+        this: FastifyInstance,
+        req: FastifyRequest,
+        _reply: FastifyReply
+      ) {
+        try {
+          await req.jwtVerify();
+        } catch (err) {
+          if (err instanceof Error) {
+            throw new AppError(`Unauthorized, ${err.message}`, 401);
+          }
+          throw new AppError('Unauthorized', 401);
+        }
+      }
+    );
+  },
+  {
+    name: 'auth-middleware'
+  }
+);
+
+declare module 'fastify' {
+  export interface FastifyInstance {
+    authenticate(req: FastifyRequest, reply: FastifyReply): Promise<void>;
+  }
+}
