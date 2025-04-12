372858d48a0ef8b5aa3e95ca2bffba474b04a303
Andrey Dantas <andreydantasvf@gmail.com>
2025-04-12 01:18:19 -0300

    Feat: add user interface/entities

--- src/domain/validations/user.validation.ts
+++ src/domain/validations/user.validation.ts
@@ -2,5 +2,7 @@ import { z } from 'zod';
 
 export const userSchema = z.object({
   email: z.string().email('Email inválido').min(1, 'Email é obrigatório'),
-  name: z.string().min(1, 'Nome é obrigatório')
+  name: z.string().min(1, 'Nome é obrigatório'),
+  password: z.string().min(1, 'Senha é obrigatória'),
+  googleId: z.string().optional()
 });
