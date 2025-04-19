6957ddc38b30c73a4471e62b1db88ec05e8689f1
Andrey Dantas <andreydantasvf@gmail.com>
2025-04-19 10:20:17 -0300

    Feat: create an user admin with seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,14 +3,40 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-  const roles = [{ name: 'user' }, { name: 'admin' }];
+  const roles = [
+    { id: 1, name: 'user' },
+    { id: 2, name: 'admin' }
+  ];
+
+  const adminsUsers = [
+    {
+      name: 'Admin CaminhoDev',
+      email: 'admin@caminhodev.com',
+      password: 'admin',
+      roleId: 2
+    }
+  ];
 
   for (const role of roles) {
     await prisma.role.upsert({
       where: { name: role.name },
       update: {},
       create: {
+        id: role.id,
         name: role.name
+      }
+    });
+  }
+
+  for (const adminUser of adminsUsers) {
+    await prisma.user.upsert({
+      where: { email: adminUser.email },
+      update: {},
+      create: {
+        name: adminUser.name,
+        email: adminUser.email,
+        password: adminUser.password,
+        roleId: adminUser.roleId
       }
     });
   }
