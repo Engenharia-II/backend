a0de4a51c54928c78c849de86f5f00f5abeb06ea
Andrey Dantas <andreydantasvf@gmail.com>
2025-04-19 12:16:25 -0300

    Feat: add usersRoutes on index

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ import App from '@/infrastructure/webserver/server';
 import SessionsRoute from '@/application/routes/sessions.route';
 import { errorHandler } from './infrastructure/webserver/error-handler';
 import AuthPlugin from './infrastructure/plugins/auth.plugin';
+import UsersRoute from '@/application/routes/users.routes';
 
 export const app = new App({
   plugins: [AuthPlugin],
-  routes: [SessionsRoute]
+  routes: [SessionsRoute, UsersRoute]
 });
 
 errorHandler(app.app);
