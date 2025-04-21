25042d0961ce20e2516d57620b117e5f6d5b36f6
Andrey Dantas <andreydantasvf@gmail.com>
2025-04-21 17:42:32 -0300

    Feat: create subject id validation

--- src/domain/validations/subject.validation.ts
+++ src/domain/validations/subject.validation.ts
@@ -4,3 +4,7 @@ export const subjectSchema = z.object({
   name: z.string().min(1, 'Nome é obrigatório'),
   description: z.string().min(1, 'Descrição é obrigatória')
 });
+
+export const subjectIdSchema = z.object({
+  id: z.string().uuid('ID inválido').min(1, 'ID é obrigatório')
+});
