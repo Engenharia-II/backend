89fe16d25a52a8470f2925e2699c974d8f7419f0
Andrey Dantas <andreydantasvf@gmail.com>
2025-04-23 01:28:15 -0300

    Feat: create topic controller

---
+++ src/application/controllers/topics/topic.controller.ts
@@ -0,0 +1,103 @@
+import {
+  createTopic,
+  deleteTopic,
+  getTopicById,
+  listTopics,
+  updateTopic
+} from '@/application/services/topics/topic.service';
+import { checkIFUserIsAdmin } from '@/application/services/user/user.service';
+import { TopicInterface } from '@/domain/interfaces/topics.interface';
+import { FastifyReply, FastifyRequest } from 'fastify';
+
+class TopicController {
+  static async create(
+    request: FastifyRequest,
+    reply: FastifyReply
+  ): Promise<TopicInterface> {
+    try {
+      const { name, description, position, subjectId } =
+        request.body as TopicInterface;
+      await checkIFUserIsAdmin(request.user.id);
+      const topic = await createTopic({
+        name,
+        description,
+        position,
+        subjectId
+      });
+      return reply.status(201).send({
+        message: 'Tópico criado com sucesso',
+        topic
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async getById(
+    request: FastifyRequest,
+    reply: FastifyReply
+  ): Promise<TopicInterface> {
+    try {
+      const { id } = request.params as { id: string };
+      const topic = await getTopicById(id);
+      return reply.status(200).send(topic);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async list(
+    _request: FastifyRequest,
+    reply: FastifyReply
+  ): Promise<TopicInterface[]> {
+    try {
+      const topics = await listTopics();
+      return reply.status(200).send(topics);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async update(
+    request: FastifyRequest,
+    reply: FastifyReply
+  ): Promise<TopicInterface> {
+    try {
+      const { id } = request.params as { id: string };
+      const { name, description, position, subjectId } =
+        request.body as TopicInterface;
+      await checkIFUserIsAdmin(request.user.id);
+      const topic = await updateTopic({
+        id,
+        name,
+        description,
+        position,
+        subjectId
+      });
+      return reply.status(200).send({
+        message: 'Tópico atualizado com sucesso',
+        topic
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async delete(
+    request: FastifyRequest,
+    reply: FastifyReply
+  ): Promise<void> {
+    try {
+      const { id } = request.params as { id: string };
+      await checkIFUserIsAdmin(request.user.id);
+      await deleteTopic(id);
+      return reply.status(200).send({
+        message: 'Tópico deletado com sucesso'
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+export default TopicController;
